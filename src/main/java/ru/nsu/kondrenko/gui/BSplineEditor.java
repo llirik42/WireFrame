4049667381b4a87856fc5c8a33a87c173215e32c
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-04-24 11:21:40 +0700

    Добавил рисование координатных осей

--- src/main/java/ru/nsu/kondrenko/gui/BSplineEditor.java
+++ src/main/java/ru/nsu/kondrenko/gui/BSplineEditor.java
@@ -1,5 +1,6 @@
 package ru.nsu.kondrenko.gui;
 
+import org.decimal4j.util.DoubleRounder;
 import ru.nsu.kondrenko.controller.BSplineMouseController;
 import ru.nsu.kondrenko.model.*;
 
@@ -12,9 +13,11 @@ public class BSplineEditor extends JPanel implements BSplineEditorContextListene
     private static final int CURVE_POINT_RADIUS = 10;
     private static final int CURVE_POINT_DIAMETER = 2 * CURVE_POINT_RADIUS;
     private final BSplineEditorContext context;
+    private final DoubleRounder rounder;
 
     public BSplineEditor(BSplineEditorContext context, BSplineMouseController controller) {
         this.context = context;
+        this.rounder = new DoubleRounder(1);
         context.addListener(this);
         setBackground(Color.WHITE);
         setPreferredSize(new Dimension(100, 100));
@@ -37,7 +40,6 @@ public class BSplineEditor extends JPanel implements BSplineEditorContextListene
         super.paintComponent(g);
         drawCurvePoints(g);
         drawAxes(g);
-        //drawBSpline(g);
     }
 
     private void drawCurvePoints(Graphics g) {
@@ -52,93 +54,55 @@ public class BSplineEditor extends JPanel implements BSplineEditorContextListene
         }
     }
 
-    private void drawBSpline(Graphics g) {
-//        final int pointsCount = context.getPoints().size();
-//        final double step = 1.0 / context.getPolylinesNumber();
-//
-//        final double[][] matrixValues = {
-//                {-1.0, 3.0, -3.0, 1.0},
-//                {3.0, -6.0, 3.0, 0.0},
-//                {-3.0, 0.0, 3.0, 0.0},
-//                {1.0, 4.0, 1.0, 0.0}
-//        };
-//        final SimpleMatrix matrix = new SimpleMatrix(matrixValues).divide(6);
-//
-//        final double[] tValues = {0.0, 0.0, 0.0, 1.0};
-//        final SimpleMatrix tMatrix = new SimpleMatrix(tValues);
-//        tMatrix.reshape(1, 4);
-//
-//        final var points = context.getPoints();
-//        for (int i = 0; i < pointsCount - 3; i++) {
-//            final double[][] pointsValues = {
-//                    {points.get(i).getRealPoint().getX(), points.get(i).getRealPoint().getY()},
-//                    {points.get(i + 1).getRealPoint().getX(), points.get(i + 1).getRealPoint().getY()},
-//                    {points.get(i + 2).getRealPoint().getX(), points.get(i + 2).getRealPoint().getY()},
-//                    {points.get(i + 3).getRealPoint().getX(), points.get(i + 3).getRealPoint().getY()},
-//            };
-//            SimpleMatrix pointsMatrix = new SimpleMatrix(pointsValues);
-//            pointsMatrix.reshape(2, 4);
-//            pointsMatrix = pointsMatrix.mult(matrix);
-//            pointsMatrix.reshape(4, 2);
-//            final SimpleMatrix prevMatrixPoint = tMatrix.mult(pointsMatrix);
-//
-//            DoublePoint prevPoint = new DoublePoint(prevMatrixPoint.get(0), prevMatrixPoint.get(1));
-//            prevPoint.minus(new DoublePoint(360, 490)); //переходим в координаты центра
-//            prevPoint.scale(1); // учитываем приближение
-//
-//            IntPoint prevMousePoint = Utils.realToMouseScale(prevPoint, context);
-//
-//            for (int j = 1; j <= context.getPolylinesNumber(); j++){
-//                final double t = (j * step);
-//                final double[] currentTValues = {
-//                        t * t * t,
-//                        t * t,
-//                        t,
-//                        1
-//                };
-//                SimpleMatrix currentMatrix = new SimpleMatrix(currentTValues);
-//                SimpleMatrix currentMatrixPoint = currentMatrix.transpose().mult(pointsMatrix);
-//                DoublePoint currentPoint = new DoublePoint(currentMatrixPoint.get(0), currentMatrixPoint.get(1));
-//                currentPoint.minus(new DoublePoint(0, 0)); //переходим в координаты центра
-//                currentPoint.scale(1); // учитываем приближение
-//                final IntPoint currentMousePoint = Utils.realToMouseScale(currentPoint, context);
-//
-//                g.drawLine(
-//                        prevMousePoint.getX(),
-//                        prevMousePoint.getY(),
-//                        currentMousePoint.getX(),
-//                        currentMousePoint.getY()
-//                );
-//
-//                prevMousePoint = currentMousePoint;
-//            }
-//        }
-
-
-
-
-//        for (int i = 0; i < pointsCount - 1; i++) {
-//            final IntPoint p1 = context.getPoints().get(i).getMousePoint();
-//            final IntPoint p2 = context.getPoints().get(i + 1).getMousePoint();
-//            g.drawLine(
-//                    p1.getX(),
-//                    p1.getY(),
-//                    p2.getX(),
-//                    p2.getY()
-//            );
-//        }
-    }
-
     private void drawAxes(Graphics g) {
-        final int yOfXAxes = 720 / 2;
-        final int xOfYAxes = 1280 / 2;
-
+        final IntPoint centerPoint = Utils.realToMouseScale(new DoublePoint(0, 0), context);
+        final int yOfXAxes = centerPoint.getY();
+        final int xOfYAxes = centerPoint.getX();
         g.drawLine(0, yOfXAxes, context.getWidth(), yOfXAxes);
         g.drawLine(xOfYAxes, 0, xOfYAxes, context.getHeight());
+
+        final int width = getWidth();
+        final int height = getHeight();
+        final Graphics2D g2d = (Graphics2D)g;
+        final int yCenter = centerPoint.getY();
+        final int xCenter = centerPoint.getX();
+        final int axesPointCount = 20;
+        final int step = width / axesPointCount;
+        final int spread = 5;
+        for (int x = xCenter + step; x < width; x += step) {
+            drawOXAxePoint(g2d, new IntPoint(x, yCenter), spread);
+        }
+        for (int x = xCenter - step; x > 0; x -= step) {
+            drawOXAxePoint(g2d, new IntPoint(x, yCenter), spread);
+        }
+        for (int y = yCenter + step; y < height; y += step) {
+            drawOYAxePoint(g2d, new IntPoint(xCenter, y), spread);
+        }
+        for (int y = yCenter - step; y > 0; y -= step) {
+            drawOYAxePoint(g2d, new IntPoint(xCenter, y), spread);
+        }
     }
 
     @Override
     public void onContextChange(BSplineEditorContext context) {
         repaint();
+    }
+
+    private void drawOXAxePoint(Graphics2D graphics2D, IntPoint point, int spread) {
+        final int x = point.getX();
+        final int y = point.getY();
+        final DoublePoint realPoint = Utils.mouseToRealScale(new IntPoint(x, y), context);
+        graphics2D.drawLine(x, y - spread, x, y + spread);
+        final String label = String.valueOf(rounder.round(realPoint.getX()));
+        graphics2D.drawString(label, x - label.length() * 3, y - 10);
+    }
+
+    private void drawOYAxePoint(Graphics2D graphics2D, IntPoint point, int spread) {
+        final int x = point.getX();
+        final int y = point.getY();
+        final DoublePoint realPoint = Utils.mouseToRealScale(new IntPoint(x, y), context);
+        graphics2D.drawLine(x - spread, y, x + spread, y);
+        final String label = String.valueOf(rounder.round(realPoint.getY()));
+        graphics2D.drawString(label, x + 10, y + 5);
     }
 }
