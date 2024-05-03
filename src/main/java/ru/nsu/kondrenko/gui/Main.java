344cb71e87c1b42e2a16f88273b544353a54c89b
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-05-03 12:01:26 +0700

    Добавил TODO

--- src/main/java/ru/nsu/kondrenko/gui/Main.java
+++ src/main/java/ru/nsu/kondrenko/gui/Main.java
@@ -2,6 +2,8 @@ package ru.nsu.kondrenko.gui;
 
 public class Main {
     public static void main(String[] args) {
+        // TODO: сделать масштабирование, перемещение и нормализацию в редакторе б-сплайнов
+        // TODO: сделать редактор б-сплайнов resizable
         new BSplineEditorWindow();
     }
 }
